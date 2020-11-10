f19d741682c3199747ae1efad1cd907af93c6d9f
Miguel Cortes <miguel.cortes@mercadolibre.com.co>
2020-11-09 20:31:01 -0500

    Added scoring service

---
+++ backends/scoring/src/main/java/co/edu/javeriana/tais2020/laurel/scoring/entities/Review.java
@@ -0,0 +1,53 @@
+package co.edu.javeriana.tais2020.laurel.scoring.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Review")
+public class Review {
+    @Transient
+    public static final String SEQUENCE_NAME = "review_sequence";
+
+    @Id
+    private Long id;
+
+    private Long itemId;
+    private int score;
+    private String comment;
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
