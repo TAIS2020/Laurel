927029039e2283b5f08d85f35e2c948b64af044a
Marko Super Computador <mguzmanmartinez@gmail.com>
2020-11-23 21:35:32 -0500

    Se realizan ajustes

--- frontends/web/src/app/pages/pagar/pagar.component.ts
+++ frontends/web/src/app/pages/pagar/pagar.component.ts
@@ -1,14 +1,11 @@
 import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Observable, of, ReplaySubject } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { ListColumn } from '../../../@fury/shared/list/list-column.model';
 import { fadeInRightAnimation } from '../../../@fury/animations/fade-in-right.animation';
 import { fadeInUpAnimation } from '../../../@fury/animations/fade-in-up.animation';
 import { Router } from '@angular/router';
+
+
+
 
 @Component({
 	selector: 'fury-pagar',
@@ -24,8 +21,18 @@ export class PagarComponent  {
 	}
    
 	agregarCarrito() {
-	 console.log("Entro...agregarCarrito.");
-	   this.router.navigate(['/carrito']);
+	 	console.log("Entro...agregarCarrito.");
+	   	this.router.navigate(['/carrito']);
+	}
+
+	RealizarPagoPse() {
+		console.log("Entro...RealizarPagoPse.");
+		//this.router.navigate(['/carrito']);
+	}
+
+	RealizarPagoTC() {
+		console.log("Entro...RealizarPagoTC.");
+		//this.router.navigate(['']);
 	}
 }
    
